a4451a88fc2d62ec9cbbe97724b081f5df679150
sam-klok <sklok11@gmail.com>
2023-05-29 21:58:12 -0700

    Adding decryption app

--- AESDecryptFiles/ProgramDecrypt.cs
+++ AESDecryptFiles/ProgramDecrypt.cs
@@ -4,10 +4,11 @@ using System.Security.Cryptography;
 using System.Text;
 using System.Reflection.Metadata;
 
-string path = Directory.GetCurrentDirectory() + @"\TestFiles";
+string path = Directory.GetCurrentDirectory() + @"\AesEcryptedFiles";
 
 var commandLineArgs = Environment.GetCommandLineArgs();
-if (commandLineArgs.Length > 1) {
+if (commandLineArgs.Length > 1)
+{
     path = commandLineArgs[1];
 }
 
@@ -16,7 +17,7 @@ Console.WriteLine($"Reading files from {path}");
 string[] files = Directory.GetFiles(path, "*.*");
 
 
-if (files == null || files.Length == 0) 
+if (files == null || files.Length == 0)
 {
     Console.WriteLine("No files to process.");
     Console.WriteLine("Please press any key..");
@@ -29,35 +30,33 @@ var aesKey = config["AesKey"];
 var aesIV = config["AesIV"];
 Console.WriteLine($"AES Key={aesKey} , IV={aesIV}");
 
-var pathEncrypted = path + @"\Encrypted";
-Directory.CreateDirectory(pathEncrypted);
+const string FolderDecrypted = @"\Decrypted";
+var pathDecrypted = path + FolderDecrypted;
+Directory.CreateDirectory(pathDecrypted);
 
 foreach (string fileName in files)
 {
-    EncryptFile(fileName, pathEncrypted, aesKey, aesIV);
+    DecryptFile(fileName, pathDecrypted, aesKey, aesIV);
 }
 
-void EncryptFile(string inputFileName, string pathEncrypted, string aesKey, string aesIV)
+void DecryptFile(string inputFileName, string pathDecrypted, string aesKey, string aesIV)
 {
-    #pragma warning disable SYSLIB0021
+#pragma warning disable SYSLIB0021
     using (var aes = new AesManaged())
-    #pragma warning restore SYSLIB0021
+#pragma warning restore SYSLIB0021
     {
 
         var fileInfo = new FileInfo(inputFileName);
-        var outputFileName = pathEncrypted + @"\" 
-            + Path.GetFileNameWithoutExtension(Path.GetRandomFileName()) 
-            + Guid.NewGuid().ToString()
-            + fileInfo.Extension;
+        var outputFileName = pathDecrypted + @"\" + fileInfo.Name;
 
         using (FileStream fStream = new FileStream(outputFileName, FileMode.Create))
         {
             byte[] key = Encoding.UTF8.GetBytes(aesKey);
             byte[] IV = Encoding.UTF8.GetBytes(aesIV);
 
-            using (ICryptoTransform aesEncrypt = aes.CreateEncryptor(key, IV))
+            using (ICryptoTransform aesDecrypt = aes.CreateDecryptor(key, IV))
             {
-                using(var cryptoStream = new CryptoStream(fStream, aesEncrypt, CryptoStreamMode.Write))
+                using (var cryptoStream = new CryptoStream(fStream, aesDecrypt, CryptoStreamMode.Write))
                 {
                     using (FileStream fileInputStream = new FileStream(inputFileName, FileMode.Open, FileAccess.Read))
                     {
@@ -73,9 +72,9 @@ void EncryptFile(string inputFileName, string pathEncrypted, string aesKey, stri
 
         }
 
-        Console.WriteLine($"{Path.GetFileName(inputFileName)} \\Encrypted\\{Path.GetFileName(outputFileName)}");
+        Console.WriteLine($"{Path.GetFileName(inputFileName)} \\{FolderDecrypted}\\{Path.GetFileName(outputFileName)}");
     }
-    
+
 }
 
 Console.WriteLine("Please press any key..");
--- readme.txt
+++ readme.txt
@@ -1,3 +1,12 @@
-Setting up .Net 6.0 console application to AES encrypt files..
+Setting up two .Net 6.0 console applications to AES encrypt and decrypt files..
 
 See examples of normal and encrypted files in folders \TestFiles and \AesEcryptedFiles
+
+Some documentation is here:
+AesManaged Class - System.Security.Cryptography
+https://learn.microsoft.com/en-us/dotnet/api/system.security.cryptography.aesmanaged?view=net-7.0
+
+
+Note: second version should probably contain MAC, and be written with newer version of the library, like below.
+AES-Encrypt-then-MAC a large file with .NET
+https://stackoverflow.com/questions/38623335/aes-encrypt-then-mac-a-large-file-with-net
